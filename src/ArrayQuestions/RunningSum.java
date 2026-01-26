91f0b95daa337da3bdd6e47f3de166f37a0da911
sushil-rindhe <sushilrindhe29@gmail.com>
2026-01-26 14:34:41 +0530

    running sum

---
+++ src/ArrayQuestions/RunningSum.java
@@ -0,0 +1,25 @@
+package ArrayQuestions;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class RunningSum {
+    public  void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int length = sc.nextInt();
+        int[] arr = new int[length];
+
+        for(int i = 0; i< length; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println(Arrays.toString(runningSum(arr, length)));
+    }
+    public static int[] runningSum(int[] nums, int length) {
+        for(int i = 1; i < length; i++){
+            nums[i] = nums[i] + nums[i-1];
+        }
+        
+        return nums;
+    }
+}
